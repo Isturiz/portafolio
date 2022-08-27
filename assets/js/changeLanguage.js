5d88a0637f626f2c70c938325e71efec83f45e6f
Mauricio Isturiz <mauricioisturizriera@hotmail.com>
2022-08-26 21:37:17 -0400

    add save language to refresh

--- assets/js/changeLanguage.js
+++ assets/js/changeLanguage.js
@@ -1,11 +1,12 @@
-const buttons = document.getElementById('btn-lang');
+const btnLang = document.getElementById('btn-lang');
 const textsToChange = document.querySelectorAll('[data-section]');
-
+const languages = ['es', 'en']
 
 const changeLanguage = async language => {
+  
   const requestJson = await fetch(`./assets/json/${language}.json`);
   const texts = await requestJson.json();
-
+  
   for (const textToChange of textsToChange) {
     const section = textToChange.dataset.section;
     const value = textToChange.dataset.value;
@@ -13,9 +14,27 @@ const changeLanguage = async language => {
   }
 };
 
-buttons.addEventListener('click', e => {
+load()
+
+btnLang.addEventListener('click', e => {
   const language = e.target.dataset.language;
   if (language) {
     changeLanguage(language);
+    store(language)
   }
 });
+
+function load() {
+
+  const lang = localStorage.getItem('language')
+
+  if(!lang) {
+    store('es')
+  } else {
+    changeLanguage(lang);
+  }
+}
+
+function store(value) {
+  localStorage.setItem('language', value)
+}
