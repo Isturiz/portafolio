f131a9d9773ca88d6b16d93fd2d8630eb8647668
Mauricio Isturiz <mauricioisturizriera@hotmail.com>
2022-09-01 17:31:01 -0400

    new style theme and changeTheme.js renewed

--- assets/js/changeTheme.js
+++ assets/js/changeTheme.js
@@ -1,37 +1,40 @@
 const btnTheme = document.querySelector('#btn-theme')
 const body = document.querySelector('body')
-const themes = ["lightMode", "roseMode"]
 
-
-let index = 0;
 load();
 
+
 btnTheme.addEventListener ('click', e => {
-  if (index >= themes.length) { index = 0}
-
-  const actualMode = body.classList[0]
-
-  body.classList.remove(actualMode)
-  body.classList.add(themes[index])
-  localStorage.setItem('theme', themes[index])
-  index++
-  localStorage.setItem('index', index.toString(10))
+  body.classList.toggle('darkmode')
+  /* 
+  store(body.classList.contains('darkmode'))
+   */
+  
+  if (body.classList.contains('darkmode')) {
+    localStorage.setItem('darkmode', 'true')
+  } else {
+    localStorage.setItem('darkmode', 'false')
+  }
 });
 
 function load() {
 
-  const theme = localStorage.getItem('theme')
-  console.log(theme);
+  const darkmode = localStorage.getItem('darkmode')
+  console.log(darkmode);
 
-  if (!theme) {
-    index = 1
-    localStorage.setItem('theme', 'lightMode')
-    body.classList.add(themes[index])
-    index++
+  if (darkmode === 'false') {
+    store('false')
+    console.log('!darkmode')
 
-  } else {
-    index = parseInt(localStorage.getItem('index'), 10)
-    body.classList.add(theme)
 
+  } else if (darkmode === 'true'){
+    console.log('darkmode')
+    body.classList.add('darkmode')
   }
+}
+
+function store(value) {
+
+  localStorage.setItem('darkmode', value.toString())
+
 }
