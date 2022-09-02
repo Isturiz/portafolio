29370528ba3fd56060584ea4278ebdd2e17fd9b8
Mauricio Isturiz <mauricioisturizriera@hotmail.com>
2022-09-02 02:58:11 -0400

    first version burger menu

--- assets/js/burgerMenu.js
+++ assets/js/burgerMenu.js
@@ -1,11 +1,9 @@
-const burgerMenu = document.querySelector('.burger-menu');
+const open = document.querySelector('#open');
+const close = document.querySelector('#close')
+
+const burgerMenu = document.querySelector('#burger-menu'); 
 const links = document.querySelectorAll('.nav-link')
 const nav = document.querySelector('#nav-container')
-
-
-burgerMenu.addEventListener ('click', e => {
-  console.log('work');
-});
 
 links.forEach((Element) => {
   Element.addEventListener('click', () => {
@@ -13,8 +11,28 @@ links.forEach((Element) => {
   });
 });
 
+burgerMenu.addEventListener("click", () => {
+  toggleButtons();
+  openOrClose("", "icon_none");
+  console.log('work');
+});
+
+
 function toggleButtons() {
   nav.classList.toggle('menu-show')
+}
+
+//nameClassBtn, clases que debe de tener en el value
+function openOrClose(nameClassBtn, nameClassNone) {
+  //variable auxiliar encargada de guardar la clase para verificar si es un icono o el otro
+  const aux = open.classList.value;
+  if (aux === nameClassBtn) {
+      open.classList.add(nameClassNone);
+      close.classList.remove(nameClassNone);
+  } else {
+      open.classList.remove(nameClassNone);
+      close.classList.add(nameClassNone);
+  }
 }
 
 
