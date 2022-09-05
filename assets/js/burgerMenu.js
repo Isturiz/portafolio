d2c9b9d2c5daa2151525fc0525c66dd115d59c51
Mauricio Isturiz <mauricioisturizriera@hotmail.com>
2022-09-05 00:36:39 -0400

    realign and delete unnecessary files

--- assets/js/burgerMenu.js
+++ assets/js/burgerMenu.js
@@ -1,39 +1,37 @@
-const open = document.querySelector('#open');
-const close = document.querySelector('#close')
+(function(){
+	const list = document.querySelector('.menu__links');
+	const menu = document.querySelector('.menu__hamburguer');
 
-const burgerMenu = document.querySelector('#burger-menu'); 
-const links = document.querySelectorAll('.nav-link')
-const navMenu = document.querySelector('#nav-menu')
-
-links.forEach((Element) => {
-  Element.addEventListener('click', () => {
-    toggleButtons();
-    openOrClose("", "icon_none");
-  });
-});
-
-burgerMenu.addEventListener("click", () => {
-  toggleButtons();
-  openOrClose("", "icon_none");
-});
+	const link = document.querySelectorAll('.menu__link')
 
 
-function toggleButtons() {
-  navMenu.classList.toggle('menu-show')
-}
-
-//nameClassBtn, clases que debe de tener en el value
-function openOrClose(nameClassBtn, nameClassNone) {
-  //variable auxiliar encargada de guardar la clase para verificar si es un icono o el otro
-  const aux = open.classList.value;
-  if (aux === nameClassBtn) {
-      open.classList.add(nameClassNone);
-      close.classList.remove(nameClassNone);
-  } else {
-      open.classList.remove(nameClassNone);
-      close.classList.add(nameClassNone);
-  }
-}
+	const open = document.querySelector('#open');
+	const close = document.querySelector('#close')
 
 
+	link.forEach((Element) => {
+		Element.addEventListener('click', () => {
+			openOrClose("", "icon_none");
+			list.classList.toggle('menu__links--show');
+		});
+	});
+
+	menu.addEventListener('click', ()=> {
+		openOrClose("", 'icon_none');
+		list.classList.toggle('menu__links--show');
+	});
+
+	function openOrClose(nameClassBtn, nameClassNone) {
+		//variable auxiliar encargada de guardar la clase para verificar si es un icono o el otro
+		const aux = open.classList.value;
+		if (aux === nameClassBtn) {
+				open.classList.add(nameClassNone);
+				close.classList.remove(nameClassNone);
+		} else {
+				open.classList.remove(nameClassNone);
+				close.classList.add(nameClassNone);
+		}
+	}
+
+})();
 
